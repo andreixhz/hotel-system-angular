81c1813d59e00158aedca8896a3d319632d0efae
Andrei Gonçalves <andrei.goncalves@modalgr.com.br>
2021-11-26 00:05:01 -0300

    feat: server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('./dist/pim-hotel'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: '/dist/pim-hotel/' }),
+    res.sendFile('index.html', { root: 'dist/pim-hotel/' }),
 );
 
 app.listen(process.env.PORT || 8080);
